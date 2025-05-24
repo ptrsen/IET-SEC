d47ccae73630450f7754c5f978f87d01b26bcd50
ptrsen <ptrsenst@gmail.com>
2025-05-24 03:45:09 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo "Installing Visual Studio Code..."
 
+echo " Installing kali tools... "
+sudo apt-get install -y kali-linux-top10
+
+echo " Installing Visual Studio Code... "
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
 sudo apt-get install -y code
 
-echo "VSCode installed"
+echo " VSCode installed "
